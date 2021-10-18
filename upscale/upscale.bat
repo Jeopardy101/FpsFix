e262f9dba4045f6053756d2c5fe437194cb4aae4
Jeopardy101 <68516328+kartemowych@users.noreply.github.com>
2021-10-18 16:26:49 -0400

    Update upscale.bat

--- upscale/upscale.bat
+++ upscale/upscale.bat
@@ -1,4 +1,6 @@
 :Loop
 timeout 1
-for %%a in ("*.mp4") do ffmpeg -i "%%a" -vf scale=3840:2160:flags=neighbor -preset fast -r 60 -profile high -rc constqp -qp 13 -c:a:0 copy -b:a 384k "upscaled\%%~na upscaled.mp4"
-if not %errorlevel% equ 0 goto :Loop
+for %%a in ("*.mp4") do (
+	ffmpeg -i "%%a" -vf scale=3840:2160:flags=neighbor -preset fast -r 60 -profile high -rc constqp -qp 13 -c:a:0 copy -b:a 384k "upscaled\%%~na upscaled.mp4"
+	if not %errorlevel% equ 0 goto :Loop
+)
